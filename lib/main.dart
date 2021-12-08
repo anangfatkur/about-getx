e21c9c471d8c288f666c003d946781bbd7dca92d
anangfatkur <anangfatkurrr@gmail.com>
2021-12-08 09:50:58 +0700

    penambahan kode Getx reactive

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
+// import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import './controller/counter_controller.dart';
-import './controller/orang_controller.dart';
+// import './controller/counter_controller.dart';
+// import './controller/orang_controller.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 // ignore: must_be_immutable
+// class MyApp extends StatelessWidget {
+//   // final orangController = Get.put(OrangController());
+
+//   // final counterController = Get.put(CounterController());
+
+//   MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(),
+//         body: Center(
+//           // child: GetX<CounterController>(
+//           child: GetBuilder<CounterController>(
+//             init: CounterController(),
+//             builder: (controller) => Text(
+//               // "Angka ${controller.counter.value}",
+//               "Angka ${controller.counter}",
+//               style: const TextStyle(fontSize: 35),
+//             ),
+//           ),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             // orangController.changeUppercase();
+//             // counterController.increment();
+//             Get.find<CounterController>().increment();
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//YOUTUBE: GETX UNIQUE ID
 class MyApp extends StatelessWidget {
-  // final orangController = Get.put(OrangController());
-
-  // final counterController = Get.put(CounterController());
-
+  // final countC = Get.put(CountController());
   MyApp({Key? key}) : super(key: key);
 
   @override
@@ -20,25 +54,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          // child: GetX<CounterController>(
-          child: GetBuilder<CounterController>(
-            init: CounterController(),
-            builder: (controller) => Text(
-              // "Angka ${controller.counter.value}",
-              "Angka ${controller.counter}",
-              style: const TextStyle(fontSize: 35),
-            ),
-          ),
+        body: GetX<CountController>(
+          init: CountController(),
+          builder: (c) {
+            return Text(
+              "Angka ${c.count}",
+              style: const TextStyle(
+                fontSize: 35,
+              ),
+            );
+          },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // orangController.changeUppercase();
-            // counterController.increment();
-            Get.find<CounterController>().increment();
-          },
+          // onPressed: () => countC.add(),
+          onPressed: () => Get.find<CountController>().add(),
         ),
       ),
     );
+  }
+}
+
+class CountController extends GetxController {
+  var count = 0.obs;
+
+  void add() {
+    count++;
   }
 }
